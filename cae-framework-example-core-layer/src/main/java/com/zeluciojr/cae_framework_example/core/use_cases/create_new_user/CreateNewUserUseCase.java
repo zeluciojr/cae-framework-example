a839b7981714c375a068fbf6c602f4ce90d941e2
Zé Lúcio Jr <joselucioalmeidajunior@gmail.com>
2024-05-08 11:55:44 -0300

    some comments for learners

--- cae-framework-example-core-layer/src/main/java/com/zeluciojr/cae_framework_example/core/use_cases/create_new_user/CreateNewUserUseCase.java
+++ cae-framework-example-core-layer/src/main/java/com/zeluciojr/cae_framework_example/core/use_cases/create_new_user/CreateNewUserUseCase.java
@@ -4,5 +4,21 @@ import com.cae.use_cases.specifics.functions.FunctionUseCase;
 import com.zeluciojr.cae_framework_example.core.use_cases.create_new_user.io.inputs.CreateNewUserUseCaseInput;
 import com.zeluciojr.cae_framework_example.core.use_cases.create_new_user.io.outputs.CreateNewUserUseCaseOutput;
 
+/**
+ * <h1> An example of FunctionUseCase.
+ *
+ * <p> Its execution method is the FunctionUseCase::execute, which accepts input and supplies output.
+ * Its input must extend the UseCaseInput type, just like any other Use Case type that has Input.
+ * Its output is free not to extend anything. </p>
+ *
+ * <p> The actual implementation with its internal rules is located at the CreateNewUserUseCaseImplementation::applyInternalLogic method. </p>
+ *
+ * <p> Before the cae-framework calls the FunctionUseCase::applyInternalLogic method,
+ * it will call the UseCaseInput::validateProperties: if the input object's attributes are complacent with their annotation requirements,
+ * the cae-framework will call the FunctionUseCase::applyInternalLogic method. </p>
+ *
+ * <p> If the Use Case is protected, the UseCase::handleAuthorization method will also be called.
+ * Since this isn't a protected Use Case, it will only call the input validation. </p>
+ */
 public abstract class CreateNewUserUseCase extends FunctionUseCase<CreateNewUserUseCaseInput, CreateNewUserUseCaseOutput> {
 }
--- cae-framework-example-core-layer/src/main/java/com/zeluciojr/cae_framework_example/core/use_cases/retrieve_all_inactive_users/RetrieveAllInactiveUsersUseCase.java
+++ cae-framework-example-core-layer/src/main/java/com/zeluciojr/cae_framework_example/core/use_cases/retrieve_all_inactive_users/RetrieveAllInactiveUsersUseCase.java
@@ -4,6 +4,18 @@ import com.cae.use_cases.authorization.annotations.ProtectedUseCase;
 import com.cae.use_cases.specifics.suppliers.SupplierUseCase;
 import com.zeluciojr.cae_framework_example.core.use_cases.retrieve_all_inactive_users.io.outputs.RetrieveAllInactiveUsersUseCaseOutput;
 
-@ProtectedUseCase(scope = {"retrieve-inactives:users"})
+/**
+ * <h1> An example of SupplierUseCase.
+ *
+ * <p> Its execution method is the SupplierUseCase::execute, which has no input and supplies output.
+ * Its output is free not to extend any type. </p>
+ *
+ * <p> The actual implementation with its internal rules is located at the RetrieveAllInactiveUsersUseCase::applyInternalLogic method. </p>
+ *
+ * <p> Before the cae-framework calls the SupplierUseCase::applyInternalLogic method, if the Use Case is protected,
+ * the UseCase::handleAuthorization method will be called. Once this is a protected Use Case, it will actually check whether or not the
+ * UseCaseExecutionCorrelation's Actor has the required scopes to execute the use case. </p>
+ */
+@ProtectedUseCase(scope = {"read-all-inactive:users"})
 public abstract class RetrieveAllInactiveUsersUseCase extends SupplierUseCase<RetrieveAllInactiveUsersUseCaseOutput> {
 }
