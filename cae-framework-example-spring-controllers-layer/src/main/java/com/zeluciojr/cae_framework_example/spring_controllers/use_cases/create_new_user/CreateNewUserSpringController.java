b216392c882099c0fb8df0adc6dc7b58f926ed06
Zé Lúcio Jr <joselucioalmeidajunior@gmail.com>
2024-05-08 17:51:00 -0300

    versioning endpoints

--- cae-framework-example-spring-controllers-layer/src/main/java/com/zeluciojr/cae_framework_example/spring_controllers/use_cases/create_new_user/CreateNewUserSpringController.java
+++ cae-framework-example-spring-controllers-layer/src/main/java/com/zeluciojr/cae_framework_example/spring_controllers/use_cases/create_new_user/CreateNewUserSpringController.java
@@ -6,19 +6,30 @@ import com.zeluciojr.cae_framework_example.core.use_cases.create_new_user.Create
 import com.zeluciojr.cae_framework_example.core.use_cases.create_new_user.io.inputs.CreateNewUserUseCaseInput;
 import com.zeluciojr.cae_framework_example.core.use_cases.create_new_user.io.outputs.CreateNewUserUseCaseOutput;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/v1/users")
 public class CreateNewUserSpringController {
 
-    private static final CreateNewUserUseCase USE_CASE = CreateNewUserUseCaseAssembler.SINGLETON_ASSEMBLER.getDefaultAssembledInstance();
+    private static final CreateNewUserUseCase USE_CASE_V1 = CreateNewUserUseCaseAssembler.V1;
+    private static final CreateNewUserUseCase USE_CASE_V2 = CreateNewUserUseCaseAssembler.V2;
 
-    @PostMapping
-    public ResponseEntity<CreateNewUserUseCaseOutput> execute(
+    @PostMapping("/v1/users")
+    public ResponseEntity<CreateNewUserUseCaseOutput> executeV1(
             @RequestHeader String correlationId,
             @RequestBody CreateNewUserUseCaseInput useCaseInput){
-        var useCaseOutput = USE_CASE.execute(useCaseInput, UseCaseExecutionCorrelation.of(correlationId));
+        var useCaseOutput = USE_CASE_V1.execute(useCaseInput, UseCaseExecutionCorrelation.of(correlationId));
+        return ResponseEntity.status(201).body(useCaseOutput);
+    }
+
+    @PostMapping("/v2/users")
+    public ResponseEntity<CreateNewUserUseCaseOutput> executeV2(
+            @RequestHeader String correlationId,
+            @RequestBody CreateNewUserUseCaseInput useCaseInput){
+        var useCaseOutput = USE_CASE_V2.execute(useCaseInput, UseCaseExecutionCorrelation.of(correlationId));
         return ResponseEntity.status(201).body(useCaseOutput);
     }
 
