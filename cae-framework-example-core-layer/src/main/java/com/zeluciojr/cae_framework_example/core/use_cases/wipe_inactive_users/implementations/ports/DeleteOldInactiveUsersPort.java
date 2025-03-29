3ca961a5ac7052fad1963329116e89414ad354cd
Zé Lúcio Jr. <joselucioalmeidajunior@gmail.com>
2025-03-29 18:31:23 -0300

    everything goes bybye

--- cae-framework-example-core-layer/src/main/java/com/zeluciojr/cae_framework_example/core/use_cases/wipe_inactive_users/implementations/ports/DeleteOldInactiveUsersPort.java
+++
@@ -1,8 +0,0 @@
-package com.zeluciojr.cae_framework_example.core.use_cases.wipe_inactive_users.implementations.ports;
-
-import com.cae.ports.specifics.consumers.ConsumerPort;
-
-import java.time.LocalDateTime;
-
-public abstract class DeleteOldInactiveUsersPort extends ConsumerPort<LocalDateTime> {
-}
--- cae-framework-example-core-layer/src/main/java/com/zeluciojr/cae_framework_example/core/use_cases/wipe_inactive_users/implementations/ports/FetchNumberOfInactiveUsersPort.java
+++
@@ -1,6 +0,0 @@
-package com.zeluciojr.cae_framework_example.core.use_cases.wipe_inactive_users.implementations.ports;
-
-import com.cae.ports.specifics.suppliers.SupplierPort;
-
-public abstract class FetchNumberOfInactiveUsersPort extends SupplierPort<Long> {
-}
